ab2b1c890ab293c331c111d1a08c7cc531cca316
aozp73 <aozp73@naver.com>
2023-02-12 12:11:07 +0900

    글쓰기 - Service 완료

--- src/main/java/shop/mtcoding/blogv1_2/service/BoardService.java
+++ src/main/java/shop/mtcoding/blogv1_2/service/BoardService.java
@@ -1,10 +1,13 @@
 package shop.mtcoding.blogv1_2.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import shop.mtcoding.blogv1_2.customException.ex.CustomApiException;
 import shop.mtcoding.blogv1_2.dto.board.BoardReq.BoardSaveReqDto;
 import shop.mtcoding.blogv1_2.model.BoardRepository;
+import shop.mtcoding.blogv1_2.util.HtmlParse;
 
 @RequiredArgsConstructor
 @Service
@@ -13,8 +16,12 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     public void 게시글등록(BoardSaveReqDto boardSaveReqDto, int principalId) {
-
-        // boardRepository.insert(null, null, null, principalId)
+        String thumbnail = HtmlParse.thumbnail(boardSaveReqDto.getContent());
+        try {
+            boardRepository.insert(boardSaveReqDto.getTitle(), boardSaveReqDto.getContent(), thumbnail, principalId);
+        } catch (Exception e) {
+            throw new CustomApiException("일시적인 서버문제가 발생했습니다", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
