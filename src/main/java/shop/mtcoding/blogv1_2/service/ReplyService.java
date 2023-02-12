3b9e2699112234bc94bc00426b542c0d75024d51
aozp73 <aozp73@naver.com>
2023-02-12 15:03:37 +0900

    댓글삭제 - Service 완료

--- src/main/java/shop/mtcoding/blogv1_2/service/ReplyService.java
+++ src/main/java/shop/mtcoding/blogv1_2/service/ReplyService.java
@@ -24,6 +24,21 @@ public class ReplyService {
     }
 
     public void 댓글삭제(int replyId, int principalId) {
+        // 댓글 존재유무
+        Reply replyPS = replyRepository.findById(replyId);
+        if (replyPS == null) {
+            throw new CustomApiException("삭제할 게시물이 존재하지 않습니다");
+        }
 
+        // 댓글작성자, 로그인유저 비교
+        if (replyPS.getUserId() != principalId) {
+            throw new CustomApiException("댓글삭제 권한이 없습니다", HttpStatus.FORBIDDEN);
+        }
+
+        try {
+            replyRepository.deleteById(replyId);
+        } catch (Exception e) {
+            throw new CustomApiException("일시적인 서버문제가 발생했습니다", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
