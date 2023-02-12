c880b8f9b1682da0c58c6a3412efb4fb87e3dda8
aozp73 <aozp73@naver.com>
2023-02-12 09:44:07 +0900

    initial commit

---
+++ src/main/java/shop/mtcoding/blogv1_2/BlogV12Application.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blogv1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogV12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogV12Application.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/blogv1_2/BlogV12ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blogv1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogV12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
