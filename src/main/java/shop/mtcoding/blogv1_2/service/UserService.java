c0d17dc2c61fa0f5be2b108b5230dedab6b1cceb
aozp73 <aozp73@naver.com>
2023-02-12 10:40:59 +0900

    회원가입 - Controller 완료

---
+++ src/main/java/shop/mtcoding/blogv1_2/service/UserService.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blogv1_2.service;
+
+import org.springframework.stereotype.Service;
+
+import shop.mtcoding.blogv1_2.dto.user.UserReq.UserJoinReqDto;
+
+@Service
+public class UserService {
+
+    public void 회원가입(UserJoinReqDto userJoinReqDto) {
+
+    }
+}
